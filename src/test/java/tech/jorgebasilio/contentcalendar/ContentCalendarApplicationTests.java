40b3d976e574d83c0777c7412f7b8a2c6c85bef5
Jbasilio55 <j.l.basilio55@gmail.com>
2023-05-23 22:51:50 -0400

    starter files for content-calendar (spring initializer)

---
+++ src/test/java/tech/jorgebasilio/contentcalendar/ContentCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.jorgebasilio.contentcalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentCalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
