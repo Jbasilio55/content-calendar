31cec8c45a9c72938475c7439fcfc2a16a129f00
Jbasilio55 <j.l.basilio55@gmail.com>
2023-05-25 09:54:36 -0400

    completed CRUD mapping

--- src/main/java/tech/jorgebasilio/contentcalendar/controller/ContentController.java
+++ src/main/java/tech/jorgebasilio/contentcalendar/controller/ContentController.java
@@ -1,17 +1,51 @@
 package tech.jorgebasilio.contentcalendar.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import tech.jorgebasilio.contentcalendar.model.Content;
 import tech.jorgebasilio.contentcalendar.repository.ContentCollectionRepository;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/aoi/content")
+@RequestMapping("/api/content")
 public class ContentController {
 
-    private ContentCollectionRepository repository;
+    private final ContentCollectionRepository repository;
 
-    public ContentController(){
-        repository = new ContentCollectionRepository();
+    public ContentController(ContentCollectionRepository repository) {
+        this.repository = repository;
     }
 
+    //make a request and find all the pieces of content in the system
+    @GetMapping("")
+    public List<Content> findAll(){
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Content findById(@PathVariable Integer id){
+        return repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Content not found!"));
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("")
+    public void create(@RequestBody Content content){
+        repository.save(content);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{id}")
+    public void update(@RequestBody Content content, @PathVariable Integer id){
+        if(!repository.existsById(id)){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Content not found");
+        }
+        repository.save(content);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Integer id){
+        repository.delete(id);
+    }
 }
--- src/main/java/tech/jorgebasilio/contentcalendar/repository/ContentCollectionRepository.java
+++ src/main/java/tech/jorgebasilio/contentcalendar/repository/ContentCollectionRepository.java
@@ -1,8 +1,12 @@
 package tech.jorgebasilio.contentcalendar.repository;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Repository;
 import tech.jorgebasilio.contentcalendar.model.Content;
+import tech.jorgebasilio.contentcalendar.model.Status;
+import tech.jorgebasilio.contentcalendar.model.Type;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -10,18 +14,45 @@ import java.util.Optional;
 @Repository
 public class ContentCollectionRepository {
 
-    private final List<Content> content = new ArrayList<>();
+    private final List<Content> contentList = new ArrayList<>();
 
     public ContentCollectionRepository(){
 
     }
 
     public List<Content> findAll(){
-        return content;
+        return contentList;
     }
 
     public Optional<Content> findById(Integer id){
-        return content.stream().filter(c -> c.id().equals(id)).findFirst();
+        return contentList.stream().filter(c -> c.id().equals(id)).findFirst();
     }
 
+    public void save(Content content){
+        contentList.removeIf(c -> c.id().equals(content.id()));
+        contentList.add(content);
+    }
+
+    @PostConstruct
+    private void init(){
+        Content c = new Content(1,
+                "my first blog post",
+                "My first blog post",
+                Status.IDEA,
+                Type.ARTICLE,
+                LocalDateTime.now(),
+                null,
+                "");
+
+        contentList.add(c);
+
+    }
+
+    public boolean existsById(Integer id) {
+        return contentList.stream().filter(c -> c.id().equals(id)).count() == 1;
+    }
+
+    public void delete(Integer id) {
+        contentList.removeIf(c -> c.id().equals(id));
+    }
 }
